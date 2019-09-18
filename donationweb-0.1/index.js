f5f612f777eeffeba0ce0f2912c41b25297c4134
cecobask <baskski@gmail.com>
2019-09-18 12:40:48 +0100

    Completed lab 2.

--- donationweb-0.1/index.js
+++ donationweb-0.1/index.js
@@ -1,5 +1,5 @@
-const chalk         = require('chalk');
-const readlineSync  = require('readline-sync');
+const chalk = require('chalk');
+const readlineSync = require('readline-sync');
 
 let donations = [
     {id: 1000000, paymenttype: 'PayPal', amount: 1600, upvotes: 1},
@@ -16,10 +16,10 @@ function displayMenu() {
     console.log('');
     choice = readlineSync.keyInSelect(options, chalk.green.bold('Choose an Option?'));
 
-    if(choice+1 === 1) console.log(chalk.red.bold('Make a donation...'));
-    else if(choice+1 === 2) console.log(donations);
-    else if(choice+1 === 3) console.log(chalk.red.bold('Search for a donation...'));
-    else if(choice+1 === 4) console.log(chalk.red.bold('Delete a donation...'));
+    if (choice + 1 === 1) addDonation();
+    else if (choice + 1 === 2) console.log(donations);
+    else if (choice + 1 === 3) findDonation();
+    else if (choice + 1 === 4) deleteDonation();
     else return process.exit(0);
 
     continueYN();
@@ -33,6 +33,51 @@ function continueYN() {
         // Another key was pressed.
         process.exit(0);
     }
+}
+
+function getByValue(array, id) {
+    let result = array.filter(obj => obj.id === id);
+    return result ? result[0] : null; // or undefined
+}
+
+function addDonation() {
+    //Randomly generate an id
+    let id = Math.floor((Math.random() * 10000000) + 1);
+    let currentSize = donations.length;
+    let paymentType = readlineSync.question("Please, enter payment type: ");
+    let paymentAmount = readlineSync.questionInt("Please, enter donation amount: ");
+
+    //Add a new donation to our list
+    const donation = {id: id, paymenttype: paymentType, amount: paymentAmount, upovotes: 3};
+    donations.push(donation);
+
+    if (donations.length > currentSize) console.log(chalk.bold("Successfully added donation!"));
+    else console.log(chalk.bold("Something went wrong :("));
+}
+
+function findDonation() {
+    const id = readlineSync.questionInt("Please enter id of the donation you are looking for: ");
+    const donation = getByValue(donations, id);
+    !donation ? console.log(chalk.bold("No donation with this id."))
+        : console.log(donation);
+}
+
+function remove(array, element) {
+    const index = array.indexOf(element);
+
+    if (index !== -1) {
+        array.splice(index, 1);
+        console.log(chalk.bold('Successfully deleted donation:'));
+        console.log(element);
+    }
+}
+
+function deleteDonation() {
+    const id = readlineSync.questionInt("Please enter id of the donation you want to delete: ");
+    const donation = getByValue(donations, id);
+
+    !donation ? console.log(chalk.bold("No donation with this id."))
+        : remove(donations, donation);
 }
 
 displayMenu();
